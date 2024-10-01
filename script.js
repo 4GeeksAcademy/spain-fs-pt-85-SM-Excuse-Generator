5d435603985d66a2fb9c1e8d984fd4d05a8a6c13
unduashe <unduashe@outlook.es>
2024-10-01 18:52:09 +0000

    unión html y hoja de script

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+window.onload = function() {
+    generadorExcusas();
+};
+
 let posiblesExcusas = {
-    culpable: ["a", "b"],
-    como: ["a", "b"],
-    objetoDestrozado: ["a", "b"],
-    cuando: ["a", "b"]
+    culpable: ["Mi perro ", "Mi abuelo ", "Mi coche ", "Mi delegado de clase favorito ", "Mi casa ", "Mi orangután "],
+    como: ["se ha comido ", "ha sufrido una combustión espontánea sobre ", "ha defecado profusamente sobre ", "se ha tumbado sobre ", "ha empezado a hacer una plancha abdominal sobre ", "ha ascendido a un plano superior cuando se encontraba sobre "],
+    objetoDestrozado: ["mis deberes ", "mi pierna ", "mi ordenador ", "el ladrillo de mi madre ", "Francia ", "los planos de mi edificio ", "la comisaría de policías de al lado de mi casa "],
+    cuando: ["ayer.", "esta madrugada.", "desde los albores de la humanidad.", "cuando llegue a casa.", "cuando mi planta, Eustaquio, se teleportó a Bielorrusia."]
 };
 
 function generadorExcusas (){
     let excusa = "";    
-    for (let i = 0; i < posiblesExcusas.length; i++){
-            let numeroAleatorio = Math.floor(Math.random() * posiblesExcusas[i].length);
-            excusa += posiblesExcusas[i][numeroAleatorio];
-    }
-    return excusa;
+    excusa += posiblesExcusas.culpable[Math.floor(Math.random() * posiblesExcusas.culpable.length)];
+    excusa += posiblesExcusas.como[Math.floor(Math.random() * posiblesExcusas.como.length)];
+    excusa += posiblesExcusas.objetoDestrozado[Math.floor(Math.random() * posiblesExcusas.objetoDestrozado.length)];
+    excusa += posiblesExcusas.cuando[Math.floor(Math.random() * posiblesExcusas.cuando.length)];
+    
+    document.getElementById("generadorExcusas").innerText = excusa;
 }
 
-generadorExcusas();
