a309aff11ae57412632e364721027d55729a57d6
unduashe <unduashe@outlook.es>
2024-10-02 19:05:49 +0000

    paso de obj a arrays y de uso de innertext a innerhtml

--- src/app.js
+++ src/app.js
@@ -2,59 +2,46 @@ window.onload = function() {
   generadorExcusas();
 };
 
-let posiblesExcusas = {
-  culpable: [
-    "Mi perro ",
-    "Mi abuelo ",
-    "Mi coche ",
-    "Mi delegado de clase favorito ",
-    "Mi casa ",
-    "Mi orangután "
-  ],
-  como: [
-    "se ha comido ",
-    "ha sufrido una combustión espontánea sobre ",
-    "ha defecado profusamente sobre ",
-    "se ha tumbado sobre ",
-    "ha empezado a hacer una plancha abdominal sobre ",
-    "ha ascendido a un plano superior cuando se encontraba sobre "
-  ],
-  objetoDestrozado: [
-    "mis deberes ",
-    "mi pierna ",
-    "mi ordenador ",
-    "el ladrillo de mi madre ",
-    "Francia ",
-    "los planos de mi edificio ",
-    "la comisaría de policías de al lado de mi casa "
-  ],
-  cuando: [
-    "ayer.",
-    "esta madrugada.",
-    "desde los albores de la humanidad.",
-    "cuando llegue a casa.",
-    "cuando mi planta, Eustaquio, se teleportó a Bielorrusia."
-  ]
-};
+let culpable = [
+  "Mi perro ",
+  "Mi abuelo ",
+  "Mi coche ",
+  "Mi delegado de clase favorito ",
+  "Mi casa ",
+  "Mi orangután "
+];
+let como = [
+  "se ha comido ",
+  "ha sufrido una combustión espontánea sobre ",
+  "ha defecado profusamente sobre ",
+  "se ha tumbado sobre ",
+  "ha empezado a hacer una plancha abdominal sobre ",
+  "ha ascendido a un plano superior cuando se encontraba sobre "
+];
+let objetoDestrozado = [
+  "mis deberes ",
+  "mi pierna ",
+  "mi ordenador ",
+  "el ladrillo de mi madre ",
+  "Francia ",
+  "los planos de mi edificio ",
+  "la comisaría de policías de al lado de mi casa "
+];
+let cuando = [
+  "ayer.",
+  "esta madrugada.",
+  "desde los albores de la humanidad.",
+  "cuando llegue a casa.",
+  "cuando mi planta, Eustaquio, se teleportó a Bielorrusia."
+];
 
 function generadorExcusas() {
   let excusa = "";
+  excusa += culpable[Math.floor(Math.random() * culpable.length)];
+  excusa += como[Math.floor(Math.random() * como.length)];
   excusa +=
-    posiblesExcusas.culpable[
-      Math.floor(Math.random() * posiblesExcusas.culpable.length)
-    ];
-  excusa +=
-    posiblesExcusas.como[
-      Math.floor(Math.random() * posiblesExcusas.como.length)
-    ];
-  excusa +=
-    posiblesExcusas.objetoDestrozado[
-      Math.floor(Math.random() * posiblesExcusas.objetoDestrozado.length)
-    ];
-  excusa +=
-    posiblesExcusas.cuando[
-      Math.floor(Math.random() * posiblesExcusas.cuando.length)
-    ];
+    objetoDestrozado[Math.floor(Math.random() * objetoDestrozado.length)];
+  excusa += cuando[Math.floor(Math.random() * cuando.length)];
 
-  document.getElementById("generadorExcusas").innerText = excusa;
+  document.getElementById("generadorExcusas").innerHTML = excusa;
 }
